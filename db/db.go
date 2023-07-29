85ee96eb79e4ff45b6b92f6fd09ceb4f297f04d1
BlackBaron <140717846+backbeast@users.noreply.github.com>
2023-07-30 02:06:19 +0900

    Import packages for mongodb connection.

--- db/db.go
+++ db/db.go
@@ -1,1 +1,13 @@
+package db
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+	mongo "github.com/helios/go-sdk/proxy-libs/heliosmongo"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
