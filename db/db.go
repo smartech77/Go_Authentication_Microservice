8d3c2889c65fc9cf5959529a2bdf263c4955fa20
BlackBaron <140717846+backbeast@users.noreply.github.com>
2023-07-30 02:08:53 +0900

    Mongodb database client connection #5

--- db/db.go
+++ db/db.go
@@ -11,3 +11,23 @@ import (
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+var client *mongo.Client
+
+func Dbconnect() *mongo.Client {
+
+	clientOptions := options.Client().ApplyURI(DotEnvVariable("MONGO_URL"))
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal("⛒ Connection Failed to Database")
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("⛒ Connection Failed to Database")
+		log.Fatal(err)
+	}
+	color.Green("⛁ Connected to Database")
+	return client
+}
