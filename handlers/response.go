b77291d05a09116b2ec25e61013ad0effd60f57d
BlackBaron <140717846+backbeast@users.noreply.github.com>
2023-07-30 02:20:33 +0900

    Authorization Response #9

--- handlers/response.go
+++ handlers/response.go
@@ -8,3 +8,13 @@ import (
 	"github.com/fatih/color"
 	"tracio.com/userservice/models"
 )
+func AuthorizationResponse(msg string, writer http.ResponseWriter) {
+	type errdata struct {
+		Message string `json:"message"`
+	}
+	temp := &errdata{Message: msg}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusUnauthorized)
+	json.NewEncoder(writer).Encode(temp)
+}
